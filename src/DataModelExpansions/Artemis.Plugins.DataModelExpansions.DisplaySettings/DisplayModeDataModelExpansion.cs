b5435c6c25ac192d24c797c1de00559fa3d6f060
Cheerpipe <cheerpipe@gmail.con>
2021-06-01 16:43:59 -0400

    New profiles compatibility changes

--- src/DataModelExpansions/Artemis.Plugins.DataModelExpansions.DisplaySettings/DisplayModeDataModelExpansion.cs
+++ src/DataModelExpansions/Artemis.Plugins.DataModelExpansions.DisplaySettings/DisplayModeDataModelExpansion.cs
@@ -1,5 +1,4 @@
 using Artemis.Core;
-using Artemis.Core.DataModelExpansions;
 using Serilog;
 using System;
 using Artemis.Plugins.DataModelExpansions.DisplaySettings.DataModels;
@@ -7,10 +6,11 @@ using Microsoft.Win32;
 using System.Linq;
 using WindowsDisplayAPI;
 using WindowsDisplayAPI.DisplayConfig;
+using Artemis.Core.Modules;
 
 namespace Artemis.Plugins.DataModelExpansions.DisplaySettings
 {
-    public class DisplayModeDataModelExpansion : DataModelExpansion<DisplaySettingsDataModel>
+    public class DisplayModeDataModelExpansion : Module<DisplaySettingsDataModel>
     {
 
         private readonly ILogger _logger;
@@ -18,6 +18,7 @@ namespace Artemis.Plugins.DataModelExpansions.DisplaySettings
         public DisplayModeDataModelExpansion(PluginSettings settings, ILogger logger)
         {
             _logger = logger;
+            IsAlwaysAvailable = true;
         }
 
         private void SystemEvents_DisplaySettingsChanged(object sender, EventArgs e)

e567b2136ef14f1874488e4c6480aad79be7472f
Cheerpipe <cheerpipe@gmail.con>
2021-06-12 23:56:06 -0400

    Add AddIntOffsetAsPercentage EndPoint

--- src/DataModelExpansions/Artemis.Plugins.DataModelExpansions.DynamicExternalDataModule/DynamicExternalDataModule.cs
+++ src/DataModelExpansions/Artemis.Plugins.DataModelExpansions.DynamicExternalDataModule/DynamicExternalDataModule.cs
@@ -27,13 +27,19 @@ namespace Artemis.Plugins.Module.DynamicExternalDataModule
             _savedDynamicDataSetting = _pluginSettings.GetSetting("SavedDynamicDataSetting", new Dictionary<string, KeyValuePair<object, Type>>());
             _webServerService.AddStringEndPoint(this, "SetBoolValue", payload => ProcessValue<bool>(payload));
             _webServerService.AddStringEndPoint(this, "ToggleBoolValue", payload => ToggleBoolValue(payload, true));
-            _webServerService.AddStringEndPoint(this, "SetIntValue", payload => ProcessValue<int>(payload));
+
+            _webServerService.AddStringEndPoint(this, "SetIntValue", payload => ProcessValue<Int64>(payload));
+            _webServerService.AddStringEndPoint(this, "AddIntOffset", payload => ProcessIntOffset(payload, 0, new Int64Range(Int64.MinValue, Int64.MaxValue)));
+            _webServerService.AddStringEndPoint(this, "AddIntOffsetAsPercentage", payload => ProcessIntOffset(payload, 0, new Int64Range(0, 100)));
+
             _webServerService.AddStringEndPoint(this, "SetFloatValue", payload => ProcessValue<float>(payload));
             _webServerService.AddStringEndPoint(this, "SetStringValue", payload => ProcessValue<string>(payload));
             _webServerService.AddStringEndPoint(this, "RemoveAll", payload => RemoveAll());
+            _webServerService.AddStringEndPoint(this, "Remove", payload => Remove(payload));
             LoadSavedValues();
         }
 
+        // TODO: More param checks and log
         private bool ProcessValue<T>(string payload)
         {
             if (!ParsePayload<T>(payload, out string key, out object value))
@@ -48,14 +54,39 @@ namespace Artemis.Plugins.Module.DynamicExternalDataModule
 
         private bool ToggleBoolValue(string key, bool defaultValue)
         {
-            if (DataModel.TryGetDynamicChild<bool>(key, out DynamicChild<bool> child))
+            DynamicChild<bool> child;
+            if (DataModel.TryGetDynamicChild<bool>(key, out child))
             {
                 child.Value = !child.Value;
             }
+            else
             {
-                DataModel.AddDynamicChild<bool>(key, defaultValue);
+                child = DataModel.AddDynamicChild<bool>(key, defaultValue);
             }
-            return SaveValue<bool>(key, child.Value);
+            if (child != null)
+                return SaveValue<bool>(key, child.Value);
+            else
+                return false;
+        }
+
+        private bool ProcessIntOffset(string payload, int defaultValue, Int64Range range)
+        {
+            if (!ParsePayload<Int64>(payload, out string key, out object value))
+            {
+                // Log
+                return false;
+            }
+
+            if (DataModel.TryGetDynamicChild<Int64>(key, out DynamicChild<Int64> child))
+            {
+                child.Value = Math.Clamp(child.Value + (Int64)value, range.From, range.To);
+                return SaveValue<Int64>(key, child.Value);
+            }
+            else
+            {
+                DataModel.AddDynamicChild<Int64>(key, defaultValue);
+            }
+            return true;
         }
 
         private bool SetValue<T>(string key, object value)
@@ -80,7 +111,7 @@ namespace Artemis.Plugins.Module.DynamicExternalDataModule
         private bool SaveValue<T>(string key, object value)
         {
             _savedDynamicData = _savedDynamicDataSetting.Value;
-            _savedDynamicData[key] = new KeyValuePair<object, Type>(value, typeof(T));
+            _savedDynamicData[key] = new KeyValuePair<object, Type>((T)value, typeof(T));
             _savedDynamicDataSetting.Value = _savedDynamicData;
             _savedDynamicDataSetting.Save();
             return true;
@@ -92,20 +123,28 @@ namespace Artemis.Plugins.Module.DynamicExternalDataModule
 
             foreach (var savedData in _savedDynamicData)
             {
-                switch (Type.GetTypeCode(savedData.Value.Value))
+                try
                 {
-                    case (TypeCode.Boolean):
-                        SetValue<bool>(savedData.Key, (bool)savedData.Value.Key);
-                        break;
-                    case (TypeCode.Int32):
-                        SetValue<int>(savedData.Key, (int)savedData.Value.Key);
-                        break;
-                    case (TypeCode.Single):
-                        SetValue<float>(savedData.Key, (float)savedData.Value.Key);
-                        break;
-                    case (TypeCode.String):
-                        SetValue<string>(savedData.Key, (string)savedData.Value.Key);
-                        break;
+                    switch (Type.GetTypeCode(savedData.Value.Value))
+                    {
+                        case (TypeCode.Boolean):
+                            SetValue<bool>(savedData.Key, (bool)savedData.Value.Key);
+                            break;
+                        case (TypeCode.Int32):
+                        case (TypeCode.Int64):
+                            SetValue<Int64>(savedData.Key, (Int64)savedData.Value.Key);
+                            break;
+                        case (TypeCode.Single):
+                            SetValue<float>(savedData.Key, (float)savedData.Value.Key);
+                            break;
+                        case (TypeCode.String):
+                            SetValue<string>(savedData.Key, (string)savedData.Value.Key);
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    //Log
                 }
             }
         }
@@ -114,6 +153,15 @@ namespace Artemis.Plugins.Module.DynamicExternalDataModule
         {
             _savedDynamicData = _savedDynamicDataSetting.Value;
             _savedDynamicDataSetting.Value.Clear();
+            _savedDynamicDataSetting.Save();
+            DataModel.ClearDynamicChildren();
+        }
+
+        private void Remove(string key)
+        {
+            DataModel.RemoveDynamicChildByKey(key);
+            _savedDynamicDataSetting.Value.Remove(key);
+            _savedDynamicData = _savedDynamicDataSetting.Value;
             _savedDynamicDataSetting.Save();
             DataModel.ClearDynamicChildren();
         }
@@ -133,5 +181,17 @@ namespace Artemis.Plugins.Module.DynamicExternalDataModule
         }
         public override void Disable() { }
         public override void Update(double deltaTime) { }
+    }
+
+    public class Int64Range
+    {
+        public Int64 From { get; set; }
+        public Int64 To { get; set; }
+
+        public Int64Range(Int64 from, Int64 to)
+        {
+            From = from;
+            To = to;
+        }
     }
 }

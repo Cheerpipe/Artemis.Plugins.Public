f5b568216b0169ff241d4e130b626e7692043d9d
Cheepipe <cheerpipe@gmail.com>
2021-11-30 13:07:33 -0300

    Use device name instead of device type

--- src/Collections/Artemis.Plugins.ExtendedWebAPI/Controllers/ColorController.cs
+++ src/Collections/Artemis.Plugins.ExtendedWebAPI/Controllers/ColorController.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Artemis.Core;
 using Artemis.Core.Services;
 using EmbedIO;
 using EmbedIO.Routing;
@@ -21,16 +22,18 @@ namespace Artemis.Plugins.ExtendedWebAPI.Controllers
             _logger = logger;
         }
 
-        [Route(HttpVerbs.Get, "/extended-rest-api/get-led-color/{deviceType}/{ledId}")]
-        public async Task GetCurrentVibrantColor(string deviceType, string ledId)
+        [Route(HttpVerbs.Get, "/extended-rest-api/get-led-color/{deviceName}/{ledId}")]
+        public async Task GetCurrentVibrantColor(string deviceName, string ledId)
         {
-            if (!(Enum.TryParse(typeof(RGBDeviceType), deviceType, true, out object parsedType)))
+            ArtemisDevice device = _rgbService.Devices.FirstOrDefault(d => d.RgbDevice.DeviceInfo.DeviceName == deviceName);
+
+            if (device == null)
             {
-                string message = $"Device type {deviceType} don't exists";
+                string message = $"Device  {deviceName} don't exists";
                 _logger.Information(message);
                 throw HttpException.NotFound(message);
             }
-                
+
             if (!(Enum.TryParse(typeof(LedId), ledId, true, out object parsedLedId)))
             {
                 string message = $"Led Id {ledId} don't exists";
@@ -38,22 +41,14 @@ namespace Artemis.Plugins.ExtendedWebAPI.Controllers
                 throw HttpException.NotFound(message);
             }
 
-            var device = _rgbService.Devices.FirstOrDefault(d => d.DeviceType == (RGBDeviceType)parsedType!);
-            if (device == null)
+            var led = device.RgbDevice.Surface!.Leds.FirstOrDefault(l => l.Id == (LedId)parsedLedId!);
+
+            if (led == null)
             {
-                string message = $"Device type {ledId} not found";
+                string message = $"Led Id {ledId} don't exists in device {deviceName}";
                 _logger.Information(message);
                 throw HttpException.NotFound(message);
             }
-
-            var led = device.RgbDevice.Surface!.Leds.FirstOrDefault(l => l.Id == (LedId)parsedLedId!);
-            if (led == null)
-                if (device == null)
-                {
-                    string message = $"Led Id {ledId} not found";
-                    _logger.Information(message);
-                    throw HttpException.NotFound(message);
-                }
 
             HttpContext.Response.ContentType = "text/plain";
             await using var writer = HttpContext.OpenResponseText(new UTF8Encoding(false));

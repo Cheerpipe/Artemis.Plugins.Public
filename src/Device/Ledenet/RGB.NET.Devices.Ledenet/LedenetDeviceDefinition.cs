c80cd34d05ddf0597c0fcede78d59070f3c73a0a
Cheerpipe <cheerpipe@gmail.com>
2022-07-29 13:42:00 -0400

    Ledenet device plugin removed

--- src/Device/Ledenet/RGB.NET.Devices.Ledenet/LedenetDeviceDefinition.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RGB.NET.Devices.Ledenet
-{
-    public class LedenetDeviceDefinition
-    {
-        public string HostName { get; set; }
-        public string DeviceName { get; set; }
-    }
-}
